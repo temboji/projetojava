a8e3e002b9ef931bebac66c04755cc193e509265
Aluno03 <Aluno03@Loy>
2019-10-24 10:18:03 -0300

    Primeiro Commit

---
+++ src/java/usuario.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aluno03
+ */
+public class usuario {
+    static String nome;
+    static String email;
+    static String senha;
+    
+    static void mostrarNome(){
+        System.out.println("O nome é:" + nome);
+    }
+    static void mostrarEmail(){
+        System.out.println("O E-mail é:" + email);
+        
+    }
+    static void mostrarSenha(){
+        System.out.println("A senha é:");
+    }
+    
+}
